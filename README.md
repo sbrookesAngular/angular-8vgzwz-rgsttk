4971d6fb47496bf2f1757a46216077dcd9a2de0b
sbrookesAngular <shawna.brookes@intel.com>
2022-07-19 17:27:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8vgzwz-rgsttk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8vgzwz-rgsttk)
---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+//{ path: 'products/:productId', component: ProductDetailsComponent }
+//routes to products.ts,  gets the id from the array and routes it to the ProductListComponent
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
